386eb18170a08aa35dc61890a21a6592603d0289
Ondřej Sláma <ondra.slama1@gmail.com>
2019-10-29 13:02:09 +0100

    New rect project

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# SportBuddy
+Ořešín
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CSS/App.css";
+
+const App = () => {
+	return (
+		<div className="App">
+			<p> ahoj </p>
+		</div>
+	);
+};
+
+export default App;
