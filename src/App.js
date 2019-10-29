aa3215c0eba5a17a0ceba55d8152b5c4f5298759
Ondřej Sláma <ondra.slama1@gmail.com>
2019-10-29 13:22:31 +0100

    First component test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./CSS/App.css";
+import Header from "./Components/Header.js";
 
 const App = () => {
 	return (
 		<div className="App">
+			<Header />
 			<p> Ahoj Míšo </p>
 			<p> No ahoj Ondro </p>
 		</div>
